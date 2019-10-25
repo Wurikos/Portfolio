e0b667abace64685f4a26093d2689afbc2817ccd
Alexander <a.5ulin@yandex.ru>
2019-10-25 21:27:55 +0300

    фикс картинки слайдера

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,69 @@
+
+ <style  lang="postcss">
+ body{
+   margin: 0;
+   box-sizing: border-box;
+ }
+  header{
+    
+  }
+  li{
+    list-style: none;
+  }
+  .head__up{
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #424267;
+  }
+  .head__up-row{
+    display: flex;
+    color: white;
+  }
+  .head__down{
+    height: 80px;
+    background-color: yellow;
+  }
+  .head__nav-list{
+    display: flex;
+  }
+  .nav__item{
+    margin-right: 10px;
+    &:last-child{
+      margin-right: 0px;
+    }
+  }
+  .nav__link{
+    text-decoration: none;
+  }
+ </style>
+
+
 <template lang="pug">
-  h1 Welcome to the Vue App
+body
+  .wrapper
+    .container
+      header
+        .head__up
+          .head__up-row
+            .head__user
+              .head__user-avatar
+              .head__user-name Сулин Александр
+            .head__text Панель администрирования
+          .head__up-row
+            .head__btn
+              a(href="#").head__btn-link Выйти
+        .head__down
+          ul.head__nav-list
+            -
+              var menu = [
+                'Обо мне',
+                'Работы',
+                'Отзывы'
+              ]
+            each item in menu
+              li.nav__item
+                a(href="#").nav__link #{item}
+
 </template>
